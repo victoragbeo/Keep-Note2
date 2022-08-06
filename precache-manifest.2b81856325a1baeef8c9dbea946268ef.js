859d12b6e8b19e7b896259bfe4bf33ffb7cc871d
Victor Agbeo <75548415+victoragbeo@users.noreply.github.com>
2022-08-06 18:48:57 -0500

    Updates

---
+++ precache-manifest.2b81856325a1baeef8c9dbea946268ef.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2eb23d5f4bbbeeef4ab9719a78a9db2",
+    "url": "/Keep-Note2/index.html"
+  },
+  {
+    "revision": "451e2cf1533465894af2",
+    "url": "/Keep-Note2/static/js/2.474a504b.chunk.js"
+  },
+  {
+    "revision": "71bc421c9f6541f3de7d",
+    "url": "/Keep-Note2/static/js/main.c413ae19.chunk.js"
+  },
+  {
+    "revision": "cdd3f1905b8b7a37dae0",
+    "url": "/Keep-Note2/static/js/runtime-main.c23e9b7b.js"
+  }
+]);
